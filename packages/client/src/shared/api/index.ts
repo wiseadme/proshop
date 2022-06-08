8981d4fab46a7ef454f77d29fba36cd9317ed796
Anar <wiseadme@gmail.com>
2022-06-08 17:20:32 +0300

    correct compose

--- packages/client/src/shared/api/index.ts
+++ packages/client/src/shared/api/index.ts
@@ -1,11 +1,14 @@
 import { Rest } from '@shared/plugins/rest'
 import axios from 'axios'
 
+const baseURL = process.env.NODE_ENV === 'development' ? '/' : 'http://localhost:5000'
+
 export const rest = new Rest(axios.create({
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json'
   },
+  baseURL,
   withCredentials: false,
   maxContentLength: 50000000,
   timeout: 10000
@@ -15,6 +18,7 @@ export const file = new Rest(axios.create({
   headers: {
     'Content-Type': 'multipart/form-data'
   },
+  baseURL,
   withCredentials: false,
   maxContentLength: 50000000,
   timeout: 10000
