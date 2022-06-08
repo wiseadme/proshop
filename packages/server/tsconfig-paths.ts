d46390627d1ab4b083fbe97f8d34cc09ef4a69db
Anar Allakhverdiev <wiseadme@gmail.com>
2022-06-08 13:47:05 +0300

    correct

--- packages/server/tsconfig-paths.ts
+++ packages/server/tsconfig-paths.ts
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
     const [ path ] = tsConfig.compilerOptions.paths[key]
     acc[key] = [ `dist/${ path }` ]
     return acc
-  })
+  }, {})
 }
 
 tsconfigPaths.register({
