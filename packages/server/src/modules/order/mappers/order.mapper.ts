e3877b9ae81fc2f1a7dfbb1064f2b663239b37cd
Anar <wiseadme@gmail.com>
2023-08-12 19:32:20 +0300

    correct

--- packages/server/src/modules/order/mappers/order.mapper.ts
+++ packages/server/src/modules/order/mappers/order.mapper.ts
@@ -1,14 +1,14 @@
-import { ICustomerMongoModel, IOrder, IOrderMongoModel, IUserMongoModel } from '@proshop/types'
+import { ICustomerMongoModel, IOrder, IOrderMongoModel } from '@proshop/types'
 import { CustomerMapper } from '@modules/customer/mappers/customer.mapper'
 import { UserMapper } from '@modules/user/mappers/user.mapper'
 
 export class OrderMapper {
-    static toDomain(entity: IOrderMongoModel): IOrder {
-        const { _id } = entity
+    static toDomain(entity: IOrderMongoModel): IOrder{
+        const _id = entity?._id
         const map: Partial<IOrderMongoModel> = entity
 
-        map.customer = CustomerMapper.toDomain(map.customer as ICustomerMongoModel)
-        map.executor = map.executor ? UserMapper.toDomain(map.executor as any): null
+        map.customer = map.customer ? CustomerMapper.toDomain(map.customer as ICustomerMongoModel) : null
+        map.executor = map.executor ? UserMapper.toDomain(map.executor as any) : null
 
         delete map._id
 
@@ -19,7 +19,7 @@ export class OrderMapper {
 
     }
 
-    static toMongoModelData(domainModel: IOrder): IOrderMongoModel {
+    static toMongoModelData(domainModel: IOrder): IOrderMongoModel{
         const { id } = domainModel
         const map: Partial<IOrder> = domainModel
 
